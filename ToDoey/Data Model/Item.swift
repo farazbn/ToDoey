1224d9cde8686401204992dee7fe6164368ebf64
Faraz Badali <farazbadali@farazs-macbook-pro.local>
2019-05-22 19:40:13 +0300

    Convert to MVC design pattern.

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by faraz badali on 22.05.2019.
+//  Copyright © 2019 faraz. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
